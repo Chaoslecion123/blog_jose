7ec38cfb0fb8c2d91a5f536181fa69fe6977f9c8
luis fernando <chaoslecion71@gmail.com>
2019-12-28 10:45:37 -0500

    asdasdasd

--- config/settings/production.py
+++ config/settings/production.py
@@ -51,6 +51,8 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Media
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 AWS_DEFAULT_ACL = None
+AWS_AUTO_CREATE_BUCKET = False
+AWS_S3_FILE_OVERWRITE = True
 MEDIA_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/'
 
 # Templates
