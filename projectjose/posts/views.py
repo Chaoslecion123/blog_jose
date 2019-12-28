65edb983fb846fdbc05e20ef8820b02693b6af88
luis fernando <chaoslecion71@gmail.com>
2019-12-28 14:00:42 -0500

    code save slider

--- projectjose/posts/views.py
+++ projectjose/posts/views.py
@@ -7,3 +7,8 @@ class PostDetailView(DetailView):
     model = Post
     slug_url_kwarg = 'the_slug'
     template_name = 'core/post.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["posts"] = Post.objects.all()
+        return context
